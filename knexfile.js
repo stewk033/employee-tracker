5a5a989de0af7f5b8f1fb28b64dfb48c2abf3b52
stewk033 <kevin.c.stewart04@gmail.com>
2022-02-02 11:59:42 -0500

    incorporate seeds

---
+++ knexfile.js
@@ -0,0 +1,53 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'mysql2',
+    connection: {
+      database: 'employees',
+      user: 'root',
+      password: 'Charmeleon85!',
+    },
+    pool: {
+      min: 2,
+      max: 110
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
